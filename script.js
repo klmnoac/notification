0971a74e33f9b2b6123065d8ff7348e80517d0b5
klmnoac <klmnoac@gmail.com>
2025-04-14 21:40:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.getElementById("data-form");
 const responseDisplay = document.getElementById("response");
 
 // Replace YOUR_WEBHOOK_URL with the Web App URL you provided
-const webhookURL = "https://script.google.com/macros/s/AKfycbw83-GuDELKJgCRif9POxgpmJ0eLQqhwdlIVTMitPNQGV5ncU76UDW3RvRqUaad94L5/exec";
+const webhookURL = "https://script.google.com/macros/s/AKfycbzNeUV8vhoLtvcA9U9Yq7APmKjkaDWblcUD6yID72CbIAxm-PauoQlKhOOmDOkpjqBH/exec";
 
 form.addEventListener("submit", (e) => {
     e.preventDefault(); // Prevent form from refreshing the page
